76e4f826367af8ff05a3ef4edb0331bb14b8c94f
Roberto Benitez Monje <roberto.benitez.es@gmail.com>
2018-11-10 14:54:32 +0100

    Refactorizacion. Bean para un properties externo

--- src/main/java/guru/springframework/DiDemoApplication.java
+++ src/main/java/guru/springframework/DiDemoApplication.java
@@ -4,11 +4,14 @@ import guru.springframework.controllers.ConstructorInjectedController;
 import guru.springframework.controllers.GetterInjectedController;
 import guru.springframework.controllers.MyController;
 import guru.springframework.controllers.PropertyInjectedController;
+import guru.springframework.examplebeans.FakeDataSource;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {"guru.springframework", "guru.springframework.services"})
 public class DiDemoApplication {
 
 	public static void main(String[] args) {
@@ -16,9 +19,8 @@ public class DiDemoApplication {
 
 		MyController controller = (MyController) ctx.getBean("myController");
 
-		System.out.println(controller.hello());
-		System.out.println(ctx.getBean(PropertyInjectedController.class).sayHello());
-		System.out.println(ctx.getBean(GetterInjectedController.class).sayHello());
-		System.out.println(ctx.getBean(ConstructorInjectedController.class).sayHello());
+		FakeDataSource fakeDataSource = (FakeDataSource) ctx.getBean(FakeDataSource.class);
+
+		System.out.println("fakeDataSource: " + fakeDataSource.getUser());
 	}
 }
